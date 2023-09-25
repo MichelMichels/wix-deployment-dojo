2756e0645a7005b5276d29f8325529e9463900f4
MichelMichels <michel@miche.ls>
2023-09-25 15:20:37 +0200

    Final commit ep9

---
+++ ep9/HelloDojo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloDojo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Dojo Episode 9!");
+            Console.ReadKey();
+        }
+    }
+}
+
+
