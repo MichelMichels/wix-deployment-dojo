69187904fc4f97dbd8317b2067a407363805b008
MichelMichels <michel@miche.ls>
2023-10-03 13:29:02 +0200

    Ep18 - Code added

---
+++ ep18/BeltTest/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Win32;
+
+object? edition = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\BeltTest", "Edition", null);
+object? customer = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\BeltTest", "Customer", null);
+
+Console.WriteLine($"Welcome to the {edition} ConsoleApp1.");
+Console.WriteLine($"  Registered our {customer} customer.");
+
+Console.ReadKey();
--- ep18/README.md
+++ ep18/README.md
@@ -3,3 +3,5 @@
 <hr/>
 
 # Deployment Dojo - Episode 18: Variables End to End. Putting it all together in WiX v4
+
+This is an exercise into a reallife scenario to use all variables.
